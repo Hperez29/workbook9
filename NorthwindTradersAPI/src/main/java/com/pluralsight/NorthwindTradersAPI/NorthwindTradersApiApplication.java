36edee865fe74b20709a3ca941603a94bbdf4deb
hperez29 <perezherbie29@yahoo.com>
2025-06-17 16:27:05 -0400

    NorthWindTradersApi Updated Categories and Products

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/NorthwindTradersApiApplication.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/NorthwindTradersApiApplication.java
@@ -2,11 +2,7 @@ package com.pluralsight.NorthwindTradersAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {
-		"com.pluralsight.NorthwindTradersAPI",
-		"com.example.northwindtradersapi"
-})
+@SpringBootApplication
 public class NorthwindTradersApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindTradersApiApplication.class, args);
